933e900460423860b51c14ba329365932ef05225
Alexey Moskalev <2020102437@pnu.edu.ru>
2025-09-15 14:18:00 +0300

    refactor: header decomposition

---
+++ frontend/src/common/components/test.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  update: [update: string];
+}>();
+
+function onInput(e: Event) {
+  const target = e.target
+  if (target instanceof HTMLInputElement) {
+    emit("update", target.value);
+  }
+}
+</script>
+
+<template>
+  <input @input="onInput" />
+</template>
