b0f2d4680f04526fa96c60995f6c3a615c8193a4
Alexey Moskalev <2020102437@pnu.edu.ru>
2025-09-17 22:44:08 +0300

    feat: add dynamic layouts

--- frontend/src/layouts/HeaderLayout.vue
+++ frontend/src/layouts/HeaderLayout.vue
@@ -1,30 +1,10 @@
 <script setup lang="ts">
-import HeaderLogo from "@/modules/header/HeaderLogo.vue";
-import HeaderUser from "@/modules/header/HeaderUser.vue";
-import HeaderCart from "@/modules/header/HeaderCart.vue";
+import HeaderComponent from "@/modules/header/HeaderComponent.vue";
 </script>
 
 <template>
-  <header :class="$style.header">
-    <header-logo />
-    <header-cart :total-price="0" />
-    <header-user />
-  </header>
+  <div>
+    <header-component />
+    <slot />
+  </div>
 </template>
-
-<style module lang="scss">
-@use "@/assets/scss/ds-system/ds-colors";
-@use "@/assets/scss/ds-system/ds-shadows";
-
-.header {
-  position: relative;
-  z-index: 2;
-
-  display: flex;
-
-  padding: 0 2.12%;
-
-  background-color: ds-colors.$green-500;
-  box-shadow: ds-shadows.$shadow-light;
-}
-</style>
