4181bd50286f1e8e4bf3f9c49b679bb47e130bac
Alexey Moskalev <2020102437@pnu.edu.ru>
2025-09-24 18:37:54 +0300

    feat: add cart store content

--- frontend/src/modules/cart/cartStore.ts
+++ frontend/src/modules/cart/cartStore.ts
@@ -1,5 +1,61 @@
 import { defineStore } from "pinia";
+import pizzaIcon from "@/assets/img/product.svg";
+import colaIcon from "@/assets/img/cola.svg";
+import sauceIcon from "@/assets/img/sauce.svg";
+import potatoIcon from "@/assets/img/potato.svg";
+import { IProductItem } from "@/modules/pizza/types/IProductItem";
+import { IAdditionalItem } from "@/modules/pizza/types/IAdditionalItem";
+import { ref } from "vue";
 
 const cartStore = defineStore("cartStore", () => {
+  const cartItems = ref<IProductItem>([
+    {
+      id: "1",
+      name: "Капричоза",
+      img: pizzaIcon,
+      size: "30 см",
+      dough: "тонком",
+      sauce: "томатный",
+      fillings: ["грибы", "лук", "ветчина", "пармезан", "ананас"],
+      count: 1,
+      price: 782,
+    },
+    {
+      id: "2",
+      name: "Любимая пицца",
+      img: pizzaIcon,
+      size: "30 см",
+      dough: "тонком",
+      sauce: "томатный",
+      fillings: [
+        "грибы",
+        "лук",
+        "ветчина",
+        "пармезан",
+        "ананас",
+        "бекон",
+        "блю чиз",
+      ],
+      count: 2,
+      price: 782,
+    },
+  ]);
 
+  const extras = ref<IAdditionalItem>([
+    {
+      id: "cola",
+      img: colaIcon,
+      title: "Coca-Cola 0,5 литра",
+      price: 56,
+      count: 2,
+    },
+    { id: "sauce", img: sauceIcon, title: "Острый соус", price: 30, count: 2 },
+    {
+      id: "potato",
+      img: potatoIcon,
+      title: "Картошка из печи",
+      price: 56,
+      count: 2,
+    },
+  ]);
 });
